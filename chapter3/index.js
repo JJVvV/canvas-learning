07a4e51d7ab359741dda260ec3bb93956034ff4a
JJVvV <drowning524@gmail.com>
2018-01-01 15:07:07 +0800

    chapter one done

---
+++ chapter3/index.js
@@ -0,0 +1,51 @@
+var canvas = document.getElementById('myCanvas')
+
+var context = canvas.getContext('2d')
+// context.fillStyle  ='rgb(255, 0, 0)'
+// context.fillRect(40, 40, 10, 100)
+// context.fillRect(180, 40, 100, 100)
+// context.fillStyle = 'rgb(133, 233, 210)'
+// context.fillRect(320, 40, 100, 80)
+// 方形
+// context.fillRect(40, 40, 300, 100)
+
+// 3.3.1
+// 线条
+// context.beginPath()
+// context.moveTo(40, 40)
+// context.lineTo(80, 80)
+// context.closePath()
+// context.stroke()
+//
+// context.strokeStyle = 'rgb(255, 0, 0)'
+// context.strokeRect(40, 40, 100, 100)
+// context.strokeRect(180, 40, 100, 100)
+// context.strokeStyle = 'rgb(122, 146, 8)'
+// context.strokeRect(320, 40, 100, 100)
+//
+// // 圆形
+// context.beginPath()
+// context.arc(230, 90, 50, Math.PI/2, Math.PI, false)
+// context.fill()
+
+// context.strokeStyle = 'rgb(255, 0, 0)'
+// context.beginPath()
+// context.moveTo(40, 180)
+// context.lineTo(420, 180)
+// // context.closePath()
+// context.stroke()
+//
+// context.strokeStyle = 'rgb(0, 0, 0)'
+// // context.beginPath()
+// // context.moveTo(40, 220)
+// context.lineTo(420, 220)
+// // context.closePath()
+// context.stroke()
+
+// 清除
+context.fillRect(40, 40, 100, 100)
+context.beginPath()
+context.arc(230, 90, 50, Math.PI, Math.PI*2, false)
+context.closePath()
+context.stroke()
+context.clearRect(40, 40, 100, 100)
